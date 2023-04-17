e7033b8a9776cd80c083d9c476a2bb0567f0feaf
Zhivar <zhivarsourati@gmail.com>
2023-04-17 09:46:10 -0400

    Add the datasetinfo and also args for each module, (deleted some unncessary code as well)

--- evaluate_model.py
+++ evaluate_model.py
@@ -1,26 +1,29 @@
 import torch
 from transformers import AutoTokenizer
-
-from args import args
+import argparse
+from IPython import embed
 import utils
-from models import ProtoTEx, ProtoTEx_roberta, ProtoTEx_electra
+from models import ProtoTEx
 
 
-def main():
+def main(args):
     # preprocess the propaganda dataset loaded in the data folder. Original dataset can be found here
     # https://propaganda.math.unipd.it/fine-grained-propaganda-emnlp.html
 
     if args.architecture == "BART":
         tokenizer = AutoTokenizer.from_pretrained("ModelTC/bart-base-mnli")
-    elif args.architecture == "RoBERTa":
-        tokenizer = AutoTokenizer.from_pretrained("cross-encoder/nli-roberta-base")
-    elif args.architecture == "Electra":
-        tokenizer = AutoTokenizer.from_pretrained("howey/electra-base-mnli")
     else:
         print(f"Invalid backbone architecture: {args.architecture}")
 
-    _, _, test_dataset = utils.load_dataset(tokenizer=tokenizer)
-    adversarial_datasets = utils.load_adv_data(tokenizer=tokenizer)
+    dataset_info = utils.DatasetInfo(
+        data_dir=args.data_dir, use_max_length=args.use_max_length
+    )
+    _, _, test_dataset = utils.load_dataset(
+        dataset_info=dataset_info, data_dir=args.data_dir, tokenizer=tokenizer
+    )
+    adversarial_datasets = utils.load_adv_data(
+        dataset_info=dataset_info, data_dir=args.data_dir, tokenizer=tokenizer
+    )
 
     adversarial_dataloaders = {
         file_name: torch.utils.data.DataLoader(
@@ -44,36 +47,11 @@ def main():
                 dropout=False,
                 special_classfn=True,  # special_classfn=False, # apply dropout only on bias
                 p=1,  # p=0.75,
-                batchnormlp1=True,
-                n_classes=utils.DatasetInfo().num_classes,
-                max_length=utils.DatasetInfo().max_length,
+                batchnormlp1=args.batchnormlp1,
+                n_classes=dataset_info.num_classes,
+                max_length=dataset_info.max_length,
             )
 
-        elif args.architecture == "RoBERTa":
-            print(f"Using backone: {args.architecture}")
-            torch.cuda.empty_cache()
-            model = ProtoTEx_roberta(
-                num_prototypes=args.num_prototypes,
-                bias=False,
-                dropout=False,
-                special_classfn=True,  # special_classfn=False, # apply dropout only on bias
-                p=1,  # p=0.75,
-                batchnormlp1=True,
-                n_classes=utils.DatasetInfo().num_classes,
-                max_length=utils.DatasetInfo().max_length,
-            ).cuda()
-        elif args.architecture == "Electra":
-            print(f"Using backone: {args.architecture}")
-            model = ProtoTEx_electra(
-                num_prototypes=args.num_prototypes,
-                bias=False,
-                dropout=False,
-                special_classfn=True,  # special_classfn=False, # apply dropout only on bias
-                p=1,  # p=0.75,
-                batchnormlp1=True,
-                n_classes=utils.DatasetInfo().num_classes,
-                max_length=utils.DatasetInfo().max_length,
-            ).cuda()
         else:
             print(f"Invalid backbone architecture: {args.architecture}")
 
@@ -94,9 +72,15 @@ def main():
         # model = torch.nn.DataParallel(model)
         model = model.to(device)
 
-        total_loss, mac_prec, mac_recall, mac_f1_score, accuracy = utils.evaluate(
-            test_dl, model_new=model
-        )
+        (
+            total_loss,
+            mac_prec,
+            mac_recall,
+            mac_f1_score,
+            accuracy,
+            y_true,
+            y_pred,
+        ) = utils.evaluate(test_dl, model_new=model)
         utils.print_logs(
             None,
             "TEST SCORES",
@@ -109,9 +93,15 @@ def main():
         )
 
         for file_name, dataloader in adversarial_dataloaders.items():
-            total_loss, mac_prec, mac_recall, mac_f1_score, accuracy = utils.evaluate(
-                dataloader, model_new=model
-            )
+            (
+                total_loss,
+                mac_prec,
+                mac_recall,
+                mac_f1_score,
+                accuracy,
+                y_true,
+                y_pred,
+            ) = utils.evaluate(dataloader, model_new=model)
             utils.print_logs(
                 None,
                 f"{file_name} TEST SCORES",
@@ -125,4 +115,27 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--tiny_sample", dest="tiny_sample", action="store_true")
+    # parser.add_argument("--nli_dataset", help="check if the dataset is in nli
+    # format that has sentence1, sentence2, label", action="store_true")
+    parser.add_argument("--num_prototypes", type=int, default=50)
+    parser.add_argument("--model", type=str, default="ProtoTEx")
+    parser.add_argument("--batch_size", type=int, default=128)
+    parser.add_argument("--data_dir", type=str)
+    parser.add_argument("--model_checkpoint", type=str, default=None)
+    parser.add_argument("--use_max_length", action="store_true")
+    parser.add_argument("--batchnormlp1", action="store_true")
+
+    # Wandb parameters
+    parser.add_argument("--experiment", type=str)
+    parser.add_argument("--nli_intialization", type=str, default="Yes")
+    parser.add_argument("--none_class", type=str, default="No")
+    parser.add_argument("--curriculum", type=str, default="No")
+    parser.add_argument("--augmentation", type=str, default="No")
+    parser.add_argument("--architecture", type=str, default="BART")
+
+    args = parser.parse_args()
+
+    main(args)
